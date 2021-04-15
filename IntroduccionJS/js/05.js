3f631353277e17d3efff5f8a2e6927f3e170d042
iamsamelea <iamsamelea.code@gmail.com>
2021-04-15 22:52:31 +0200

    Aprendiendo Math

---
+++ IntroduccionJS/js/05.js
@@ -0,0 +1,17 @@
+// NÚMEROS
+
+const numeroPrueba = "100"; // El número entre comillas pierde el valor de número y se queda como texto, en la consola se ve de color negro y de esa forma se puede distinguir
+const numero1 = 100; // Este número se verá reflejado de color azul y tiene valor de número y se peude trabajar con el
+const numero2 = 200;
+const numero3 = 200.20; // En JS todos los números tienen la misma forma de trabajar, no hay que especificar si son booleanos, etc
+const numero4 = .1025;
+const numero5 = -5;
+
+console.log(numeroPrueba);
+console.log(numero1);
+console.log(numero2);
+console.log(numero1 + numero2); // Suma
+console.log(numero1 - numero2); // Resta
+console.log(numero1 * numero2); // Multiplicación
+console.log(numero1 / numero2); // División
+console.log(numero1 % numero2); // Módulo, es como una división que resta el residuo
---
+++ IntroduccionJS/js/06.js
@@ -0,0 +1,21 @@
+// OBJETO MATH
+
+let resultado;
+
+
+/**Funciones más utilizadas de Math**/ 
+
+resultado = Math.PI; // Da como resultado el número PI
+resultado = Math.round(2.5); // Redondea el número de una forma normal
+resultado = Math.ceil(2.1); // Redondea el resultado hacia arriba
+resultado = Math.floor(2.9); // Redondea el resultado hacia abajo
+resultado = Math.sqrt(144); // Da como resultado la raíz cuadrada del número
+resultado = Math.abs(-200); // Cambia el resultado del número a positivo
+resultado = Math.min(1, 5, 20, 3, 18, 23, 9); // Da como resultado el número más pequeño
+resultado = Math.max(1, 5, 20, 3, 18, 23, 9); //Da como resultado el número más grande
+resultado = Math.random(); // Da un resultado random siempre por debajo de 1
+
+
+resultado = Math.floor(Math.random() * 30); // Con este tipo de función conseguimos un número random mayor que uno porque lo multiplicamos por 30
+
+console.log(resultado);
