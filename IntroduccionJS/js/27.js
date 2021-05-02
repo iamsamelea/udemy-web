4ea9ce9108ca666b693042b4339b2c60ac4f4da9
iamsamelea <iamsamelea.code@gmail.com>
2021-05-02 20:14:49 +0200

    while y do while

---
+++ IntroduccionJS/js/27.js
@@ -0,0 +1,64 @@
+// WHILE LOOP Y DO WHILE LOOP
+
+
+
+
+//While Loop
+// let i = 0; // Indice
+
+// while( i < 10) { // Condición
+//     console.log(i);
+
+//     ++i; // Incremento
+// }
+
+
+
+// While Loop se ejecutará siempre que la condición sea verdadera, ejecuta la condición y si no es verdadera no ejecuta el código.
+//Paso 1: declaramos la variable que la inicamos en 0 ( Indice ).
+//Paso 2: utilizamos la palabra reservada while y en el parentesis ponemos la condición de que nos muestre los números menores de 10.
+//Paso 3: Le damos el incremento al acabar la condición y después de mostrarla.
+
+
+
+
+
+
+// let i = 0; // Indice
+
+// while( i <= 100) { // Condición
+
+//     if(i % 2 === 0) {
+//         console.log(`El número ${i} es PAR`);
+//     } else {
+//         console.log(`El número ${i} es IMPAR`);
+//     }
+
+//     ++i; // Incremento
+// }
+
+// Dentro de las llaves también se pueden utilizar esrtucturas de control como son por ejemplo el if y el else
+
+
+
+
+
+
+
+
+// Do While Loop
+let i = 0; // Indice
+
+do {
+    console.log(i); // Código que se ejecuta
+
+    i++;    // Incremento
+} while ( i < 10); // Condición
+
+
+
+//En el Do While Loop se ejecuta almenos el código 1 vez, y después evalua si el código se cumple.
+//Paso 1: declaramos la variable que la inicamos en 0 ( Indice ).
+//Paso 2: utilizamos la palabra reservada do y le abrimos unas llaves, que es donde se ejecutará el codigo.
+//Paso 3: Ponemos el código que se ejecuta y le damos el incremento antes de cerrar las llaves.
+//Paso 4: fuera de las llaves para finalizar utilizamos la palabra reservada while y en el parentesis ponemos la condición de que nos muestre los números menores de 10.
