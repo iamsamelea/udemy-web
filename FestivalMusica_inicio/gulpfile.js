a3ab4f36d105e3d3c19d325e2c75e27f919541be
Samuel Lara <iamsamelea.code@gmail.com>
2021-05-24 23:51:02 +0200

    gulp-sass

---
+++ FestivalMusica_inicio/gulpfile.js
@@ -0,0 +1,45 @@
+                                                    // const { series, parallel } = require('gulp'); //Paso 1
+
+                                                    // function css( done ) { //Paso 2
+                                                    //     console.log('Compilando SASS') 
+
+                                                    //     done(); 
+                                                    // }
+
+                                                    // function javascript( done ) {
+                                                    //     console.log('Compilando Javascript')
+
+                                                    //     done();
+                                                    // }
+
+                                                    // function minificarHTML( done ) {
+                                                    //     console.log('Minificando...')
+
+                                                    //     done();
+                                                    // }
+
+                                                    // exports.css = css; //Paso 3
+                                                    // exports.javascript = javascript;
+                                                    // exports.minificarHTML = minificarHTML;
+                                                    // exports.default = series( css, javascript, minificarHTML); //Paso 4
+
+                                                    // // Paso 1: se crea una variable y entre las llaves se pone la funcion {series} para seleccionar todas las funciones e ir compilandolas en orden, {parallel} hace lo mismo pero las ejecuta todas a la vez dependiendo lo rapido que se ejecute el código en cada función, se le da igual a require que lo que hace es traer la dependencia de node que queramos, en este caso gulp desde la carpeta node_modules para así poder trabajar con ella.
+
+                                                    // //Paso 2: se crea una funcion en este caso llamada css y en los parametros le pones done, abrimos las llaves y dentro escribimos un console.log con el mensaje que queramos transmitir en la consola, para finalizar ponemos un done(); para dar la función por finalizada.
+
+                                                    // //Paso 3: utilizamos la función exports para imprimir el mensaje en la terminal y seguida de un punto (.) le damos el nombre con el que quremos llamar a la función en la terminal, despues se pone un igual (=) y ponemos exactamente el mismo nombre que tiene la función.
+
+                                                    // //Paso 4: este exports.default es con la función series que lo que permite és imprimir todas las funciones a la vez sin tener que poner gulp y el nombre de cada una, con solo utilizar gulp ya se van imprimiento de una en una siempre que despues del igual escribamos la función que utilizemos, tanto {series} como {parallel} y en los parametros ponemos las demás funciones que queramos ejecutar. 
+
+const { series, src, dest } = require('gulp'); //src deja comunicar archivos y dest te deja almacenar un scss compilado
+const sass = require('gulp-sass'); //no va entre llaves porque solo existe esta función dentro de gulp-sass
+
+
+//FUNCION QUE COMPILA SASS
+function css( ) {
+    return src('src/scss/app-scss') //return devuelve la ubicación, src es la funcion de de arriba de gulp y el Array es la ubicación donde está el archivo a compilar
+        .pipe( sass() ) //.pipe hace que se lean las funciones de arriba hacia abajo
+        .pipe( dest('./build/css') ) // el ./ significa que es la carpeta actual, carpeta build y el archivo creado es un css
+}
+
+exports.css = css;
