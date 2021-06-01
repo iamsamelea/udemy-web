ffcc0f6c38aec7705118a33dbbbfb58c01438adf
Samuel Lara <iamsamelea.code@gmail.com>
2021-06-01 19:56:30 +0200

    sass en css

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -31,33 +31,60 @@
 
                                                     // //Paso 4: este exports.default es con la función series que lo que permite és imprimir todas las funciones a la vez sin tener que poner gulp y el nombre de cada una, con solo utilizar gulp ya se van imprimiento de una en una siempre que despues del igual escribamos la función que utilizemos, tanto {series} como {parallel} y en los parametros ponemos las demás funciones que queramos ejecutar. 
 
-const { series, src, dest, watch } = require('gulp'); //src deja comunicar archivos y dest te deja almacenar un scss compilado
+const { series, src, dest, watch, parallel } = require('gulp'); //src deja comunicar archivos y dest te deja almacenar un scss compilado
 const sass = require('gulp-sass'); //no va entre llaves porque solo existe esta función dentro de gulp-sass
+const imagemin = require ('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
 
 
 //FUNCION QUE COMPILA SASS
-function css( ) {
-    return src('src/img/scss/app.scss') //return devuelve la ubicación, src es la funcion de de arriba de gulp y el Array es la ubicación donde está el archivo a compilar
+
+const paths = { //se crea un objeto para alojar esta url y poder trabajar mas rapidamenre en las funciones de abajo paths.imagenes en vez de poner la ruta entera
+    imagenes: 'src/img/**/*',
+    scss: 'src/scss/**/*.scss', // * = La carpeta actual - ** = Todos los archivos con esa extensión      
+}
+
+function css() {
+    return src(paths.scss) //return devuelve la ubicación, src es la funcion de de arriba de gulp y el Array es la ubicación donde está el archivo a compilar
         .pipe( sass( {
             outputStyle: 'expanded'
         }) ) //.pipe hace que se lean las funciones de arriba hacia abajo
         .pipe( dest('./build/css') ) // el ./ significa que es la carpeta actual, carpeta build y el archivo creado es un css
 }
 
-function minificarcss( ) {
-    return src('src/img/scss/app.scss')
+function minificarcss() {
+    return src(paths.scss)
     .pipe( sass( {
         outputStyle: 'compressed'
     }) )
     .pipe( dest('./build/css') )
 }
 
-function watchArchivos() { //esta función compila automaticamente los cambios que le hagamos al archivo app.scss y lo cambia en el archivo app.css
-    watch( 'src/img/scss/**/*.scss', css ); // * = La carpeta actual - ** = Todos los archivos con esa extensión      //con la funcion watch automatizamos el proceso, en el parentesis ponemos la ruta y seguido y separado por una coma (,) le ponemos la función que quremos ejecutar automaticamente cada vez que guardemos, el asterisco le indica que seleccione todos los archivos con la extensión .scss
+function imagenes() {
+    return src(paths.imagenes)
+        .pipe( imagemin() )
+        .pipe( dest( './build/img'))
+        .pipe( notify({ message: 'Imagen Minificada'}) );
 }
+
+function versionWebp() {
+    return src(paths.imagenes)
+        .pipe( webp() )
+        .pipe( dest('./build/img') )
+        .pipe( notify( {message: 'Versión webP lista'}) );
+}
+
+function watchArchivos() { //esta función compila automaticamente los cambios que le hagamos al archivo app.scss y lo cambia en el archivo app.css
+    watch( paths.scss, css ); //con la funcion watch automatizamos el proceso, en el parentesis ponemos la ruta y seguido y separado por una coma (,) le ponemos la función que quremos ejecutar automaticamente cada vez que guardemos, el asterisco le indica que seleccione todos los archivos con la extensión .scss
+}
+
 
 
 
 exports.css = css;
 exports.minificarcss = minificarcss;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos; //Esta función hay que ejecutarla cada vez al empezar a trabajar y al acabar tan solo con darle a la basura de la terminal ya se pararia
+
+exports.default = series( css, imagenes, versionWebp, watchArchivos ) //Esta función se encarga de ejecutar todas las funciones de series
